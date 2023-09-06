602d6bc68171a8a9549ec109dc1bc1527c56954d
Mak Isabella Aiman Mak <maiman-m@u82z03s02.42kl.edu.my>
2023-09-06 17:04:00 +0800

    practiced tgetxxx

--- practice/tty.c
+++ practice/tty.c
@@ -2,8 +2,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <term.h> //or termcap.h
+#include <string.h>
+//cc -Wall -Wextra -Werror -fsanitize=address -lcurses tty.c
+//cc -Wall -Wextra -Werror -fsanitize=address -ltermcap tty.c
 
-int	main()
+int	main(int argc, char **argv)
 {
 	int	fd = open("test.txt", O_RDONLY);
 	if (dup2(STDIN_FILENO, fd) == -1)
@@ -15,5 +19,31 @@ int	main()
 		fprintf(stderr, "what is a termina-\n");
 	printf("file %s\n in %s\n out %s\n err %s\n", ttyname(fd), ttyname(STDIN_FILENO), ttyname(STDOUT_FILENO), ttyname(STDERR_FILENO));
 	printf("current slot: %i\n", ttyslot());
-	printf("SHELL=%s\nUSER=%s\nLOGNAME=%s\nHOME=%s\nMAIL=%s\n", getenv("SHELL"), getenv("USER"), getenv("LOGNAME"), getenv("HOME"), getenv("MAIL"));
+	printf("SHELL=%s\nUSER=%s\nLOGNAME=%s\nHOME=%s\nMAIL=%s\nBULLSHIT=%s\n", getenv("SHELL"), getenv("USER"), getenv("LOGNAME"), getenv("HOME"), getenv("MAIL"), getenv("BULLSHIT"));
+
+	printf("\n\n");
+
+	/*
+	 * home, lang - not sp, acc
+	 * user - err
+	 * term - acc
+	 */
+	if (argc == 2)
+	{
+		char *termtype = getenv(argv[1]);
+		if (!termtype)
+		   fprintf(stderr, "terminal type not specified\n");
+		char	*buf = malloc(sizeof(char) * 2048);
+		//static char	buf[2048];
+		int	check_getent = tgetent(buf, termtype);
+		if (check_getent < 0)
+			fprintf(stderr, "error\n");
+		else if (check_getent == 1)
+			fprintf(stderr, "accessible but what's the type\n");
+		else
+			printf("something something\n");
+
+		printf("height: %i\n", tgetnum("li"));
+		printf("width: %i\n", tgetnum("co"));
+	}
 }
