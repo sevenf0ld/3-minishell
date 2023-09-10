7d99f0d08a2a24d983da2ad7ce2fc15a29bf1f27
Mak Isabella Aiman Mak <maiman-m@u82z03s02.42kl.edu.my>
2023-09-10 22:27:29 +0800

    commented out parser

--- builtins/parse.c
+++ builtins/parse.c
@@ -6,9 +6,26 @@
 /*   By: maiman-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 17:01:08 by maiman-m          #+#    #+#             */
-/*   Updated: 2023/09/10 20:56:47 by maiman-m         ###   ########.fr       */
+/*   Updated: 2023/09/10 22:26:13 by maiman-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "build.h"
 
+/*
+what if split on pipe instead of on space
+void	parser(t_token **tokens, t_command **cmds)
+{
+	t_token	*tmp;
+
+	tmp = *tokens;
+	while (tmp != NULL)
+	{
+		if (!tmp->symbol)
+			//store it as cmd/flags/input
+		else
+			//check if it is a pipe if not word
+
+	}
+}
+*/
