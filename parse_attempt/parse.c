f461c0dcdfefcf586c05d9161436c8b852008f05
Mak Isabella Aiman Mak <maiman-m@u90z04s05.42kl.edu.my>
2023-09-11 17:42:08 +0800

    set_multi handles if flags are passed separately and if there are more than one input

--- parse_attempt/parse.c
+++ parse_attempt/parse.c
@@ -13,20 +13,58 @@
 
 #include "build.h"
 
+void	set_multi(t_split *split_cmd, t_command **c_node)
+{
+	t_split		*tmp;
+	int			i;
+	int			j;
+	
+	tmp = split_cmd;
+	i = 0;
+	j = 0;
+	while (tmp != NULL)
+	{
+		if (tmp->symbol == OPT)
+			i += 1;
+		if (tmp->symbol == MSG)
+			j += 1;
+		tmp = tmp->next;
+	}
+	if (i != 0)
+		(*c_node)->num_f = i;
+	if (j != 0)
+		(*c_node)->num_i = j;
+}
+
+//does not handle multi flags and multi inputs which needs to malloced
 void	set_cmd(char **full_cmd, t_command **c_node)
 {
-	t_command	*ptr;
 	t_split		*split_cmd;
+	t_split		*tmp;
+	t_command	*node;
 
 	split_cmd = NULL;
 	split_init(full_cmd, &split_cmd);
 	set_split_type(&split_cmd, ft_strcmp);
+	set_multi(split_cmd, c_node); 
 	
 	t_split	*sym;
 	for (sym = split_cmd; sym != NULL; sym = sym->next)
 		printf("~~~ %s ~~~ %i\n", sym->each, sym->symbol);
 
-	ptr = *c_node;
+	tmp = split_cmd;
+	node = *c_node;
+	while (tmp != NULL)
+	{
+		if (tmp->symbol == CMD)
+			node->cmd = tmp->each;
+		else if (tmp->symbol == OPT)
+			node->flags = tmp->each;
+		else if (tmp->symbol == MSG)
+			node->input = tmp->each;
+		tmp = tmp->next;
+	}
+	printf("NUM CHECK (f)%i (i)%i\n", node->num_f, node->num_i);
 }
 
 /*
