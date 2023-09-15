6c17699f16b8cefe2491bce6b2c2da5f17f22bd3
Mak Isabella Aiman Mak <maiman-m@u90z01s02.42kl.edu.my>
2023-09-15 11:51:11 +0800

    the script compiles the c files and deletes the executable & debug file

--- prototype_v1/split.sh
+++ prototype_v1/split.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
+red='\033[0;31m'
+green='\033[0;33m'
+blue='\033[0;34m'
+none='\033[m'
+
 # create an array of cmd line arguments
-args=("ls -la | cat | grep -wn c > outfile" "ls -l -a | cat | grep -w -n c > outfile" "< infile ls -l | wc -l > outfile" "echo -n -n -n" "echo -n" "ls txtfile" 'echo "$example"' "echo '$example'" "echo '\$example'" "echo build.h '|' grep b" "echo build.h | grep b"
+args=("ls -la | cat | grep -wn c > outfile"
+	"ls -l -a | cat | grep -w -n c > outfile"
+	"< infile ls -l | wc -l > outfile"
+	"echo -n -n -n"
+	"echo -n"
+	"ls txtfile"
+	'echo "$example"'
+	"echo '$example'"
+	"echo '\$example'"
+	"echo build.h '|' grep b"
+	"echo build.h | grep b"
 	)
 
 # create a variable with the name of the executable
-exe="./minishell_split"
+exe="./new_split"
+
+cc -Wall -Werror -Wextra -fsanitize=address -g3 split.c -o new_split
 
 for arg in "${args[@]}"; do
-    echo -e "$exe $arg:"
+    echo -e "$red $exe $arg: $none"
     ./"$exe" "$arg"
-	echo ""
 done
+
+rm -rf new_split*
