a82a65031f4367c2de71e52ff6b3990818a9fa0e
alexlimfl <132150229+alexlimfl@users.noreply.github.com>
2023-09-15 14:32:08 +0800

    updated double linked list convert

--- prototype_v1/d_ll_convert.c
+++ prototype_v1/d_ll_convert.c
@@ -1,20 +1,11 @@
 
-#include <unistd.h>
+#include "mini.h"
 
-typedef struct s_node
+t_token	*double_ll_convert(t_token **lst)
 {
-	int				x;
-	char			*str;
-	struct s_node	*prev;
-	struct s_node	*next;
-}t_node;
-
-
-t_node	*double_ll_convert(t_node **lst)
-{
-	t_node	*prev;
-	t_node	*curr;
-	t_node	*tail;
+	t_token	*prev;
+	t_token	*curr;
+	t_token	*tail;
 
 	if (*lst == NULL)
 		return (NULL);
@@ -30,5 +21,12 @@ t_node	*double_ll_convert(t_node **lst)
 	return (tail);
 }
 
+// typedef struct s_token
+// {
+// 	int				x;
+// 	char			*str;
+// 	struct s_token	*prev;
+// 	struct s_token	*next;
+// }t_token;
 // double_ll_convert takes in the root of the linked list and returns
 // the tail of the linked list.
