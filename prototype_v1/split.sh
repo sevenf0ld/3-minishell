32cfecd3fdb4be22036a8e4695161c6fdec31195
Mak Isabella Aiman Mak <maiman-m@u90z01s02.42kl.edu.my>
2023-09-15 12:14:47 +0800

    Revert "script checks for leaks in new_split()"
    This reverts commit 86a9e5c411f5595723affa77461644b1313c1921.

--- prototype_v1/split.sh
+++ prototype_v1/split.sh
@@ -22,13 +22,11 @@ args=("ls -la | cat | grep -wn c > outfile"
 # create a variable with the name of the executable
 exe="./new_split"
 
-cc -Wall -Werror -Wextra -g3 split.c -o new_split
-# cc -Wall -Werror -Wextra -fsanitize=address -g3 split.c -o new_split
+cc -Wall -Werror -Wextra -fsanitize=address -g3 split.c -o new_split
 
 for arg in "${args[@]}"; do
     echo -e "$red $exe $arg: $none"
-    leaks -atExit -- ./"$exe" "$arg"
-    #./"$exe" "$arg"
+    ./"$exe" "$arg"
 done
 
 rm -rf new_split*
