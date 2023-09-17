3083d2e5a44b3d9c94b85e13dce9f56499501885
Mak Isabella Aiman Mak <maiman-m@u80z03s02.42kl.edu.my>
2023-09-17 18:18:25 +0800

    Revert "Revert "ifk dmthn""
    This reverts commit f9de08577b51098b75e30cc4bdf406ff04acde30.

---
+++ prototype_v1/tokenizer_utils.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maiman-m <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 16:19:30 by maiman-m          #+#    #+#             */
+/*   Updated: 2023/09/17 17:46:35 by maiman-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mini.h"
+
+/*
+ * | > <
+ * " '
+ */
+int	is_delim(char a)
+{
+	if (a == 124 || a == 62 || a == 60)
+		return (1);
+	if (a == 34 || a == 39)
+		return (1);
+	return (0);
+}
+
+char	*handle_spaces_btwn_q(char a, char c)
+{
+	if (a == 34 && c == 34)
+		return (ft_strdup(" "));
+	if (a == 39 && c == 39)
+		return (ft_strdup(" "));
+	return (NULL);
+}
+
+int	num_sbq(char *s)
+{
+	int	i = -1;
+	int	num = 0;
+	while (s[++i] != '\0')
+	{
+		if (ft_iswhite(s[i]))
+		{
+			if (s[i - 1] == 34 && s[i + 1] == 34)
+				num += 1;
+			if (s[i - 1] == 39 && s[i + 1] == 39)
+				num += 1;
+		}
+	}
+	return (num);
+}
+
+char	**init_split_pipeline(char *s, int w_c)
+{
+	int		additional = 0;
+	char	**end;
+
+	additional = num_sbq(s) + w_c;
+	end = malloc(sizeof(char *) * (additional + 1));
+	return (end);
+}
