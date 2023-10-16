4d43357b92aa708857cdb698ef4e0709a7fd4fb3
Mak Isabella Aiman Mak <maiman-m@u81z03s03.42kl.edu.my>
2023-10-16 18:46:45 +0800

    starting the execution

--- prototype_v1/main.c
+++ prototype_v1/main.c
@@ -6,13 +6,13 @@
 /*   By: maiman-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 12:19:04 by maiman-m          #+#    #+#             */
-/*   Updated: 2023/09/17 14:17:15 by maiman-m         ###   ########.fr       */
+/*   Updated: 2023/10/16 18:46:01 by maiman-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
 
-int	main(int argc, char **argv)
+int	main(void)
 {
 	char	*type[] = {"PIPE", "OUT_RE", "IN_RE", "W_Q", "S_Q", "CMD", "OPT", "ARGS", "FILN", "LIM", "HD", "ADD", "ANON"};
 	
@@ -23,50 +23,16 @@ int	main(int argc, char **argv)
 	pipeline = NULL;
 	tok = NULL;
 	cmd = NULL;
-	/*
-	//while (1)
-	//{
+	while (1)
+	{
 		pipeline = readline("prompt> ");
 		if (pipeline && *pipeline)
 			add_history(pipeline);
 		lexer(pipeline, &tok);
+		for (t_token *dl = tok; dl != NULL; dl = dl->next)
+			fprintf(stderr, "\x1b[44m[%s]\x1b[m is of type %i which is \x1b[36m[%s]\x1b[m\n", dl->token, dl->symbol, type[dl->symbol]);
+		parser(&tok, &cmd);
 		free(pipeline);
 		pipeline = NULL;
-	//}
-	*/
-	if (argc != 2)
-		return (1);
-	
-	lexer(argv[1], &tok);
-	for (t_token *dl = tok; dl != NULL; dl = dl->next)
-		printf("\x1b[44m[%s]\x1b[m is of type %i which is \x1b[36m[%s]\x1b[m\n", dl->token, dl->symbol, type[dl->symbol]);
-		//printf("[%s] is a [%s]. end? \x1b[32m%s\x1b[m\n", dl->token, type[dl->symbol], dl->end?"true":"false");
-	
-	//parser(&tok, &cmd);
-	//for (t_token *dl = tok; dl != NULL; dl = dl->next)
-		//printf("\x1b[44m[%s]\x1b[m is of type %i which is \x1b[36m[%s]\x1b[m\n", dl->token, dl->symbol, type[dl->symbol]);
-		//printf("[%s] is a [%s]. end? \x1b[32m%s\x1b[m\n", dl->token, type[dl->symbol], dl->end?"true":"false");
-	
-	parser(&tok, &cmd);
-	
-	//for (t_command *full = cmd; full != NULL; full = full->next)
-		//printf("cmd set \x1b[33m%s\x1b[m\n", full->cmd);
-	
-	t_command *tmp;
-	for (tmp = cmd; tmp != NULL; tmp = tmp->next)
-	{
-		printf("@ [%s]\n", tmp->cmd);
-		if (tmp->flags != NULL)
-		{
-			printf("flags yes\n");
-			for (int i = 0; i < tmp->num_f; i++)
-				printf("--- {%s}\n", tmp->flags[i]);
-		}
-		if (tmp->args != NULL)
-		{
-			printf("args yes\n");
-			for (int i = 0; i < tmp->num_a; i++)
-				printf("::: {%s}\n", tmp->args[i]);
-		}
 	}
 }
