06523108cd8c7245c3f3cf383696a618985e8f3d
Mak Isabella Aiman Mak <maiman-m@u82z02s02.42kl.edu.my>
2023-10-17 18:51:42 +0800

    expanded environment variables but only '' works

--- prototype_v1/main.c
+++ prototype_v1/main.c
@@ -6,7 +6,7 @@
 /*   By: maiman-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 12:19:04 by maiman-m          #+#    #+#             */
-/*   Updated: 2023/10/17 14:37:10 by maiman-m         ###   ########.fr       */
+/*   Updated: 2023/10/17 18:49:45 by maiman-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,7 @@ int	main(int argc, char **argv)
 	
 	lexer(argv[1], &tok);
 	for (t_token *dl = tok; dl != NULL; dl = dl->next)
-	{
 		printf("\x1b[44m[%s]\x1b[m is of type %i which is \x1b[36m[%s]\x1b[m [%d]\n", dl->token, dl->symbol, type[dl->symbol], dl->exp);
-	}
 		// printf("[%s] is a [%s]. end? \x1b[32m%s\x1b[m\n", dl->token, type[dl->symbol], dl->end?"true":"false");
 
 	//parser(&tok, &cmd);
@@ -50,26 +48,21 @@ int	main(int argc, char **argv)
 		//printf("[%s] is a [%s]. end? \x1b[32m%s\x1b[m\n", dl->token, type[dl->symbol], dl->end?"true":"false");
 	
 	parser(&tok, &cmd);
-	
-	//for (t_command *full = cmd; full != NULL; full = full->next)
-		//printf("cmd set \x1b[33m%s\x1b[m\n", full->cmd);
-	
 	t_command *tmp;
 	for (tmp = cmd; tmp != NULL; tmp = tmp->next)
 	{
 		printf("@ [%s]\n", tmp->cmd);
 		if (tmp->flags != NULL)
 		{
-			printf("flags yes\n");
+			//printf("flags yes\n");
 			for (int i = 0; i < tmp->num_f; i++)
 				printf("--- {%s}\n", tmp->flags[i]);
 		}
 		if (tmp->args != NULL)
 		{
-			printf("args yes\n");
+			//printf("args yes\n");
 			for (int i = 0; i < tmp->num_a; i++)
 				printf("::: {%s}\n", tmp->args[i]);
 		}
 	}
-	printf("NO TOKENS LEFT %p\n", tok);
 }
