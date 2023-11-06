36a37746f528ccf5a3b0eb1ab04b589f6c4c54a5
sevenf0ld <isabellaaimanmak@gmail.com>
2023-11-06 16:53:05 +0800

    removed loop from main bc its fucking up in_re, preparing to merge

--- prototype_v1/main.c
+++ prototype_v1/main.c
@@ -6,7 +6,7 @@
 /*   By: folim <folim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 12:19:04 by maiman-m          #+#    #+#             */
-/*   Updated: 2023/11/04 19:29:08 by folim            ###   ########.fr       */
+/*   Updated: 2023/11/06 16:52:26 by maiman-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int	main(void)
 	pipeline = NULL;
 	tok = NULL;
 	cmd = NULL;
+	/*
 	while (1)
 	{
 		pipeline = readline("prompt> ");
@@ -35,5 +36,17 @@ int	main(void)
 			dup2_err(restore_stdout, STDOUT_FILENO);
 			close_err(restore_stdout);
 		}
+	}
+	*/
+	pipeline = readline("prompt> ");
+	if (ft_strcmp(pipeline, ""))
+	{
+		restore_stdout = dup_err(STDOUT_FILENO);
+		add_history(pipeline);
+		lexer(pipeline, &tok);
+		parser(&tok, &cmd);
+		n_builtins(&cmd);
+		dup2_err(restore_stdout, STDOUT_FILENO);
+		close_err(restore_stdout);
 	}
 }
