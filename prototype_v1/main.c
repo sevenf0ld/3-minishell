a99f45f98d78228c4b0548b17546dedd529bcdde
sevenf0ld <isabellaaimanmak@gmail.com>
2023-11-20 05:06:19 +0800

    redirect_io_pipe() behaves oddly although assign_pipe_ends() and print_inode() shows the correct redirection

--- prototype_v1/main.c
+++ prototype_v1/main.c
@@ -6,23 +6,11 @@
 /*   By: folim <folim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 12:19:04 by maiman-m          #+#    #+#             */
-/*   Updated: 2023/11/19 23:04:13 by maiman-m         ###   ########.fr       */
+/*   Updated: 2023/11/20 03:41:47 by maiman-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
-
-#include <sys/stat.h>
-#include <errno.h>
-#include <string.h>
-
-void print_inode(int fd, char *name) {
- struct stat info;
- if (fstat(fd, &info) != 0)
-   fprintf(stderr,"fstat() error for %s %d: %s\n",name,fd,strerror(errno));
- else
-   fprintf(stderr, "╲ The inode of %s is %d\n", name, (int) info.st_ino);
-}
 
 int	main(void)
 {
@@ -40,26 +28,22 @@ int	main(void)
 		pipeline = readline("prompt> ");
 		if (ft_strcmp(pipeline, ""))
 		{
-			restore_stdout = dup_err(STDOUT_FILENO);
-			restore_stdin = dup_err(STDIN_FILENO);
-			//print_inode(restore_stdout, "restore_stdout ->");
-			//print_inode(restore_stdin, "restore_stdin ->");
 			add_history(pipeline);
 			lexer(pipeline, &tok);
+			restore_stdout = dup_err(STDOUT_FILENO);
+			restore_stdin = dup_err(STDIN_FILENO);
 			parser(&tok, &cmd, restore_stdout, restore_stdin);
 			/*
 			n_builtins(&cmd);
 			if (cmd_size(cmd) > 1)
 				n_builtins(&cmd->next);
+			*/
 			for (t_command *cur = cmd; cur != NULL; cur = cur->next)
 				n_builtins(&cur);
-			*/
 			dup2_err(restore_stdout, STDOUT_FILENO);
 			close_err(restore_stdout);
 			dup2_err(restore_stdin, STDIN_FILENO);
 			close_err(restore_stdin);
-			//print_inode(STDOUT_FILENO, "restored SO ->");
-			//print_inode(STDIN_FILENO, "restored SI ->");
 		}
 	}
 }
