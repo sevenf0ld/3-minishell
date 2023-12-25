cba594198b23a8b82dbd50df34e90b6bd31899f0
isabella <isabella@42>
2023-12-25 21:19:34 +0800

    exit works

--- prototype_v1/b_exit.c
+++ prototype_v1/b_exit.c
@@ -2,46 +2,52 @@
 
 void	b_exit(t_command *c_node)
 {
-	if (c_node->num_f == 0 && c_node->num_f == 0)
+	char		**end;
+	int		n;
+	long long	code;
+
+	end = NULL;
+	n = 0;
+	if (c_node->num_f == 0 && c_node->num_a == 0)
 	{
 		c_node->stat->s_code = 0;
 		return ;
 	}
-	long long	conv;
-	//int	conv;
-	if (c_node->num_f > 0)
+	end = ft_split(c_node->og, ' ');
+	while (end[n] != NULL)
+		n++;
+	code = ft_atoll(end[1]);
+	if (ft_strcmp(ft_lltoa(code), end[1]) != 0)
 	{
-		conv = ft_atoll(c_node->flags[0]);
-		//conv = ft_atoi(c_node->flags[0]);
-		if (!ft_strcmp(ft_lltoa(conv), c_node->flags[0]))
-		//if (!ft_strcmp(ft_itoa(conv), c_node->flags[0]))
-		{
-			//if (conv > 255)
-			if (conv > -255)
-				//c_node->stat->s_code = conv % 256;
-				c_node->stat->s_code = conv + 256;
-			else
-			{
-				//c_node->stat->s_code = conv;
-				printf("check neg %lli\n", (conv + 256) % (256 + 0U));
-				c_node->stat->s_code = (conv + 256) % (256 + 0U);
-				printf("U: for things smaller than -255: %u (u) or %i (i)\n", -44 % (unsigned)256, -44 % (unsigned)256);
-				printf("I: for things smaller than -255: %u (u) or %i (i)\n", -44 % 256, -44 % 256);
-			}
-			if (c_node->num_f > 1 || c_node->num_a > 0)
-			{
-				printf("minishell: exit: too many arguments\n");
-				c_node->stat->s_code = 1;
-				fprintf(stderr, "S_CODE [%d] but will not close this shell\n", c_node->stat->s_code);
-				return ;
-			}
-		}
-		else
-		{
-			printf("minishell: exit: %s: numeric argument required\n", c_node->flags[0]);
-			c_node->stat->s_code = 2;
-		}
-		fprintf(stderr, "S_CODE [%d] will close this shell if it weren't for testing purposes\n", c_node->stat->s_code);
+		printf("minishell: exit: %s: numeric argument required\n", end[1]);
+		c_node->stat->s_code = 2;
+		//exit(c_node->stat->s_code);
+		fprintf(stderr, "S_CODE \x1b[45m[%i]\x1b[m will close this shell if it weren't for testing purposes\n", c_node->stat->s_code);
 		return ;
 	}
+	else
+	{
+		if (n > 2)
+		{
+			printf("minishell: exit: too many arguments\n");
+			c_node->stat->s_code = 1;
+			fprintf(stderr, "S_CODE \x1b[42m[%i]\x1b[m but will not close this shell\n", c_node->stat->s_code);
+			return ;
+		}
+	}
+	if (code >= 0)
+	{
+		if (code > 255)
+			c_node->stat->s_code = code % 256;
+		else
+			c_node->stat->s_code = code;
+	}
+	else
+	{
+		c_node->stat->s_code = code + 256;
+		if (code <= -255)
+			c_node->stat->s_code %= (256 + 0U);
+	}
+	//exit(c_node->stat->s_code);
+	fprintf(stderr, "S_CODE \x1b[45m[%i]\x1b[m will close this shell if it weren't for testing purposes\n", c_node->stat->s_code);
 }
