54feddf8e83ae3062a48ce08b3442a344756b920
isabella <isabella@42>
2023-12-26 14:57:46 +0800

    Handle exit builtin with and without arguments
    * b_exit() handles no args, valid and invalid arguments BUT doesn't exit for TESTING PURPOSES
    * v1_exit.sh tests v1's implementation and exit status code
    * exit.sh tests bash's implementation and exit status code

--- prototype_v1/b_exit.c
+++ prototype_v1/b_exit.c
@@ -1,19 +1,53 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   b_exit.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: maiman-m <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/25 12:42:44 by maiman-m          #+#    #+#             */
-/*   Updated: 2023/11/25 12:45:22 by maiman-m         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "mini.h"
 
 void	b_exit(t_command *c_node)
 {
-	if (!ft_strcmp(c_node->cmd, "exit"))
-		exit(EXIT_SUCCESS);
+	char		**end;
+	int		n;
+	long long	code;
+
+	end = NULL;
+	n = 0;
+	if (c_node->num_f == 0 && c_node->num_a == 0)
+	{
+		c_node->stat->s_code = 0;
+		return ;
+	}
+	end = ft_split(c_node->og, ' ');
+	while (end[n] != NULL)
+		n++;
+	code = ft_atoll(end[1]);
+	if (ft_strcmp(ft_lltoa(code), end[1]) != 0)
+	{
+		printf("minishell: exit: %s: numeric argument required\n", end[1]);
+		c_node->stat->s_code = 2;
+		//exit(c_node->stat->s_code);
+		fprintf(stderr, "S_CODE \x1b[45m[%i]\x1b[m will close this shell if it weren't for testing purposes\n", c_node->stat->s_code);
+		return ;
+	}
+	else
+	{
+		if (n > 2)
+		{
+			printf("minishell: exit: too many arguments\n");
+			c_node->stat->s_code = 1;
+			fprintf(stderr, "S_CODE \x1b[42m[%i]\x1b[m but will not close this shell\n", c_node->stat->s_code);
+			return ;
+		}
+	}
+	if (code >= 0)
+	{
+		if (code > 255)
+			c_node->stat->s_code = code % 256;
+		else
+			c_node->stat->s_code = code;
+	}
+	else
+	{
+		c_node->stat->s_code = code + 256;
+		if (code <= -255)
+			c_node->stat->s_code %= (256 + 0U);
+	}
+	//exit(c_node->stat->s_code);
+	fprintf(stderr, "S_CODE \x1b[45m[%i]\x1b[m will close this shell if it weren't for testing purposes\n", c_node->stat->s_code);
 }
