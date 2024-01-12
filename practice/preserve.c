6f96774ce5aa17e609d3f11f5de05ae7c72db054
isabella <isabella@42>
2024-01-12 15:13:58 +0800

    handled odd spacing and counted correctly

--- practice/preserve.c
+++ practice/preserve.c
@@ -43,6 +43,20 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (sub);
 }
 
+int	all_whitespace(char *s, int start, int end)
+{
+    int	i;
+
+    i = start;
+    while (s[i] != '\0' && i < end)
+    {
+        if (s[i] != 32)
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
 void    check_double_quote(char c, int index, bool *d)
 {
     if (c == 34 && !*d)
@@ -78,12 +92,12 @@ void    check_single_quote(char c, int index, bool *s)
 int iterate_until_closing(char *s, int start, char c)
 {
     int i = start + 1;
-    fprintf(stderr, "opening at \x1b[34mindex %i (%c)\x1b[m or \x1b[34m index %i (%c)\x1b[m\n", start, s[start], i, s[i]);
+    //fprintf(stderr, "opening at \x1b[34mindex %i (%c)\x1b[m or \x1b[34m index %i (%c)\x1b[m\n", start, s[start], i, s[i]);
     while (s[i] != '\0')
     {
         if (s[i] == c)
         {
-            fprintf(stderr, "closing at \x1b[34mindex %i (%c)\x1b[m\n", i, s[i]);
+            //fprintf(stderr, "closing at \x1b[34mindex %i (%c)\x1b[m\n", i, s[i]);
             return i;
         }
         i++;
@@ -93,17 +107,17 @@ int iterate_until_closing(char *s, int start, char c)
 
 int main()
 {
-    char *single_str = "i am not in quotes except for \'1. one\' \'2. two\' \'3. three\', they are the exception \'4. four\'";
-    char *double_str = "i am not in quotes except for \"1. one\" \"2. two\" \"3. three\", they are the exception \"4. four\"";
-    char *d_in_s_str = "i am not in quotes except for \'\"1. one\"\' \'\"2. two\"\' \'\"3. three\"\', they are the exception \'\"4. four\"\'";
-    char *s_in_d_str = "i am not in quotes except for \"\'1. one\'\" \"\'2. two\'\" \"\'3. three\'\", they are the exception \"\'5. five\'\"";
+    char *single_str = "i am       not in quotes except for   \'1. one\' \'2. two\' \'3. three\', they are the exception \'4. four\'";
+    char *double_str = "i am not in     quotes except for   \"1. one\" \"2. two\" \"3. three\", they are the exception \"4. four\"";
+    char *d_in_s_str = "i am   not in quotes except    for \'\"1. one\"\'     \'\"2. two\"\' \'\"3. three\"\', they are the exception \'\"4.    four\"\'";
+    char *s_in_d_str = "i am not in quotes    except for \"\'1. one\'\" \"\'2.     two\'\"      \"\'3. three\'\", they are the exception \"\'5.     five\'\"";
 
-    char *str = single_str;
+    char *str = double_str;
     printf("\x1b[35mstr: %s\x1b[m\n", str);
     void (*func)(char, int, bool *) = NULL;
-    func = check_single_quote;
+    func = check_double_quote;
     (void) func;
-    char quote_to_check = 39;
+    char quote_to_check = 34;
 
     bool q = false;
     int i = 0;
@@ -111,19 +125,26 @@ int main()
     char *sub = NULL;
     int start = 0;
     int end = ft_strlen(str);
+    int num = 0;
     while (str[i] != '\0')
     {
         //func(str[i], i, &q);
         if (str[i] == quote_to_check && !q)
         {
             end = i - 1;
-            sub = ft_substr(str, start, (size_t) end - start);
-            fprintf(stderr, "   unquoted ft_substr() =\x1b[36m %s \x1b[m\n", sub);
+            if (!all_whitespace(str, start, end))
+            {
+                sub = ft_substr(str, start, (size_t) end - start);
+                num += 1;
+                fprintf(stderr, "   unquoted ft_substr() =\x1b[36m|%s|\x1b[m\n", sub);
+            }
             fprintf(stderr, "\x1b[33m   starts at %i\x1b[m\n", i);
             q = true;
             close = iterate_until_closing(str, i, quote_to_check);
             sub = ft_substr(str, i, (size_t) close - i + 1);
-            fprintf(stderr, "   quoted ft_substr() =\x1b[36m %s \x1b[m\n", sub);
+            if (sub != NULL)
+                num += 1;
+            fprintf(stderr, "   quoted ft_substr() =\x1b[36m|%s|\x1b[m\n", sub);
             i += close - i;
             fprintf(stderr, "\x1b[33m   ends at %i\x1b[m\n", i);
             continue ;
@@ -135,5 +156,5 @@ int main()
         }
         i++;
     }
-    fprintf(stderr, "\n");
+    fprintf(stderr, "number: %i\n", num);
 }
