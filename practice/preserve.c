1b77f34ac06191f5b8027fca80465c16dce1177f
isabella <isabella@42>
2024-01-13 18:14:51 +0800

    preserve() handles all imaginable test cases

--- practice/preserve.c
+++ practice/preserve.c
@@ -57,60 +57,79 @@ int iterate_until_closing(char *s, char c)
     return i;
 }
 
-int main()
+void    decide_quote(char c, bool *sq, bool *wq)
 {
-    char *single_str = "i am       not in quotes except for   \'1. one\' \'2. two\' \'3. three\', they are the exception \'4. four\'5.five";
-    char *double_str = "i am not in     quotes except for   \"1. one\" \"2. two\" \"3. three\", they are the exception \"4. four\"5.five";
-    char *d_in_s_str = "i am   not in quotes except    for \'\"1. one\"\'     \'\"2. two\"\' \'\"3. three\"\', they are the exception \'\"4.    four\"\'5.five";
-    char *s_in_d_str = "i am not in quotes    except for \"\'1. one\'\" \"\'2.     two\'\"      \"\'3. three\'\", they are the exception \"\'5.     five\'\"5.five";
-    char *mess = "\'single\' \"double\" \'\"d_in_s\"\' \"\'s_in_d\'\"";
-    char *stick = "hi\'single\'hello hello\"double\"hi hello\'\"d_in_s\"\'hello hi\"\'s_in_d\'\"hi";
-    
-    char *str = stick;
-    printf("\x1b[35mstr: %s\x1b[m\n", str);
-    char quote_to_check = 34;
+    if (c == 39 && !*sq)
+        *sq = true;
+    else if (c == 34 && !*wq)
+        *wq = true;
+    else if (c == 39 && *sq)
+        *sq = false;
+    else if (c == 34 && *wq)
+        *wq = false;
+}
 
+void    preserve(char *str)
+{
+    fprintf(stderr, "\x1b[35mstr: %s\x1b[m\n", str);
     bool q = false;
+    (void) q;
+    bool sq = false;
+    bool wq = false;
     int i = 0;
     int close = 0;
-    char *sub = NULL;
     int check;
     int start = -1;
     while (str[i] != '\0')
     {
-        //func(str[i], i, &q);
-        if (str[i] == quote_to_check && !q)
+        if (str[i] == 39 || str[i] == 34)
         {
-            q = true;
-            close = iterate_until_closing(str, quote_to_check);
+            decide_quote(str[i], &sq, &wq);
+            if (sq || wq)
+                close = iterate_until_closing(str, str[i]);
         }
-        else if (str[i] == quote_to_check && q)
-        {
-            q = false;
-        }
-        if (str[i] != 32 || (str[i] == 32 && q))
+        if (str[i] != 32 || (str[i] == 32 && (sq || wq)))
         {
             close += 1;
-            //fprintf(stderr, "|%c|    %i, %i\n", str[i], close, i);
             if (start == -1)
-            {
                 start = i;
-                //fprintf(stderr, "\x1b[42mSTART %i\x1b[m\n", start);
-            }
         }
-        else if (str[i] == 32 && !q)
+        else if (str[i] == 32 && (!wq && !sq))
         {
-            //fprintf(stderr, "%s (%i, %i)\n", str + i, close, i);
             // skip the spaces outside of quotes
             if (check != close)
             {
                 check = close;
-                //fprintf(stderr, "\x1b[41m(%i, %i) %i\x1b[m\n", close, i, check);
-                fprintf(stderr, "SUBSTR-> %s\n", ft_substr(str, start, (size_t) i - start));
+                //fprintf(stderr, "SUBSTR-> %s\n", ft_substr(str, start, (size_t) i - start));
+                continue ;
             }
+            fprintf(stderr, "SUBSTR-> %s\n", ft_substr(str, start, (size_t) i - start));
             start = -1;
         }
         i++;
     }
     fprintf(stderr, "SUBSTR-> %s\n", ft_substr(str, start, (size_t) i - start));
+
+}
+
+int main()
+{
+    char *single_str = "i am not in quotes except for \'1. one\' \'2. two\' \'3. three\', they are the exception \'4. four\'5.five";
+    char *double_str = "i am not in quotes except for \"1. one\" \"2. two\" \"3. three\", they are the exception \"4. four\"5.five";
+    char *d_in_s_str = "i am not in quotes except for \'\"1. one\"\' \'\"2. two\"\' \'\"3. three\"\', they are the exception \'\"4. four\"\'5.five";
+    char *s_in_d_str = "i am not in quotes except for \"\'1. one\'\" \"\'2. two\'\" \"\'3. three\'\", they are the exception \"\'4. four\'\"5.five";
+    char *mess = "\'single\' \"double\" \'\"d_in_s\"\' \"\'s_in_d\'\"";
+    char *stick = "hi\'single\'hello hello\"double\"hi hello\'\"d_in_s\"\'hello hi\"\'s_in_d\'\"hi";
+    char *multi = "strong \'slim shady in quotes\' \'    \', weak \"mike posner in quotes\" \"    \"";
+
+    char *str = single_str;
+    //fprintf(stderr, "\x1b[35mstr: %s\x1b[m\n", str);
+    preserve(str);
+    
+    preserve(double_str);
+    preserve(d_in_s_str);
+    preserve(s_in_d_str);
+    preserve(mess);
+    preserve(stick);
+    preserve(multi);
 }
