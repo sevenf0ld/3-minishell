347d5824a80366c61a24f65ea55bd3e2be854156
isabella <isabella@42>
2024-01-13 12:08:55 +0800

    getting the substr is a pain in the ass bc the indexes of start and end will be reset each time

--- practice/preserve.c
+++ practice/preserve.c
@@ -43,20 +43,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (sub);
 }
 
-int	all_whitespace(char *s, int start, int end)
-{
-    int	i;
-
-    i = start;
-    while (s[i] != '\0' && i < end)
-    {
-        if (s[i] != 32)
-            return (0);
-        i++;
-    }
-    return (1);
-}
-
 void    check_double_quote(char c, int index, bool *d)
 {
     if (c == 34 && !*d)
@@ -89,15 +75,13 @@ void    check_single_quote(char c, int index, bool *s)
         fprintf(stderr, "%c", c);
 }
 
-int iterate_until_closing(char *s, int start, char c)
+int iterate_until_closing(char *s, char c)
 {
-    int i = start + 1;
-    //fprintf(stderr, "opening at \x1b[34mindex %i (%c)\x1b[m or \x1b[34m index %i (%c)\x1b[m\n", start, s[start], i, s[i]);
+    int i = 0;
     while (s[i] != '\0')
     {
         if (s[i] == c)
         {
-            //fprintf(stderr, "closing at \x1b[34mindex %i (%c)\x1b[m\n", i, s[i]);
             return i;
         }
         i++;
@@ -107,10 +91,10 @@ int iterate_until_closing(char *s, int start, char c)
 
 int main()
 {
-    char *single_str = "i am       not in quotes except for   \'1. one\' \'2. two\' \'3. three\', they are the exception \'4. four\'";
-    char *double_str = "i am not in     quotes except for   \"1. one\" \"2. two\" \"3. three\", they are the exception \"4. four\"";
-    char *d_in_s_str = "i am   not in quotes except    for \'\"1. one\"\'     \'\"2. two\"\' \'\"3. three\"\', they are the exception \'\"4.    four\"\'";
-    char *s_in_d_str = "i am not in quotes    except for \"\'1. one\'\" \"\'2.     two\'\"      \"\'3. three\'\", they are the exception \"\'5.     five\'\"";
+    char *single_str = "i am       not in quotes except for   \'1. one\' \'2. two\' \'3. three\', they are the exception \'4. four\'5.five";
+    char *double_str = "i am not in     quotes except for   \"1. one\" \"2. two\" \"3. three\", they are the exception \"4. four\"5.five";
+    char *d_in_s_str = "i am   not in quotes except    for \'\"1. one\"\'     \'\"2. two\"\' \'\"3. three\"\', they are the exception \'\"4.    four\"\'5.five";
+    char *s_in_d_str = "i am not in quotes    except for \"\'1. one\'\" \"\'2.     two\'\"      \"\'3. three\'\", they are the exception \"\'5.     five\'\"5.five";
 
     char *str = double_str;
     printf("\x1b[35mstr: %s\x1b[m\n", str);
@@ -123,38 +107,44 @@ int main()
     int i = 0;
     int close = 0;
     char *sub = NULL;
+    int check;
     int start = 0;
-    int end = ft_strlen(str);
-    int num = 0;
+    int end = 0;
     while (str[i] != '\0')
     {
         //func(str[i], i, &q);
         if (str[i] == quote_to_check && !q)
         {
-            end = i - 1;
-            if (!all_whitespace(str, start, end))
-            {
-                sub = ft_substr(str, start, (size_t) end - start);
-                num += 1;
-                fprintf(stderr, "   unquoted ft_substr() =\x1b[36m|%s|\x1b[m\n", sub);
-            }
-            fprintf(stderr, "\x1b[33m   starts at %i\x1b[m\n", i);
             q = true;
-            close = iterate_until_closing(str, i, quote_to_check);
-            sub = ft_substr(str, i, (size_t) close - i + 1);
-            if (sub != NULL)
-                num += 1;
-            fprintf(stderr, "   quoted ft_substr() =\x1b[36m|%s|\x1b[m\n", sub);
-            i += close - i;
-            fprintf(stderr, "\x1b[33m   ends at %i\x1b[m\n", i);
-            continue ;
+            close = iterate_until_closing(str, quote_to_check);
         }
-        if (str[i] == quote_to_check && q)
+        else if (str[i] == quote_to_check && q)
         {
             q = false;
-            start = i + 1;
+        }
+        if (str[i] != 32 || (str[i] == 32 && q))
+        {
+            close += 1;
+            printf("|%c|    %i, %i\n", str[i], close, i);
+        }
+        else if (str[i] == 32 && !q)
+        {
+            //fprintf(stderr, "%s (%i, %i)\n", str + i, close, i);
+            if (!start)
+                start = i;
+            if (start && !end)
+                end  = i;
+            // skip the spaces outside of quotes
+            //if (check != close)
+            //{
+            //    check = close;
+            //    fprintf(stderr, "(%i, %i) %i\n", close, i, check);
+            //    fprintf(stderr, "substr %s (start: %i, end %i)\n", ft_substr(str, start, (size_t) end - start), start, end);
+            //}
+            check = close;
+            fprintf(stderr, "(%i, %i) %i\n", close, i, check);
+            fprintf(stderr, "substr %s (start: %i, end %i)\n", ft_substr(str, start, (size_t) end - start), start, end);
         }
         i++;
     }
-    fprintf(stderr, "number: %i\n", num);
 }
