affeb66729785e07f6322a93eead365e9091d8d5
isabella <isabella@42>
2024-01-26 00:10:07 +0800

    redo

--- prototype_v1/expand_utils4.c
+++ prototype_v1/expand_utils4.c
@@ -6,24 +6,36 @@
 /*   By: maiman-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 14:34:58 by maiman-m          #+#    #+#             */
-/*   Updated: 2024/01/25 23:29:05 by maiman-m         ###   ########.fr       */
+/*   Updated: 2024/01/26 00:08:36 by maiman-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
 
-static int iterate_until_closing(char *s, char c)
+char    *till_dollar(char *s, int i)
 {
-    int i = 0;
-    while (s[i] != '\0')
-    {
-        if (s[i] == c)
-        {
-            return i;
-        }
-        i++;
-    }
-    return i;
+    char    *ret;
+    
+    if (i == 0)
+        return (NULL);
+    ret = ft_strndup(s, i);
+    //fprintf(stderr, "up to %s\n", ret);
+    return (ret);
+}
+
+char    *sub_exp(char *s)
+{
+    char    *ext;
+    char    *sub;
+    
+    ext = NULL;
+    sub = NULL;
+    ext = ft_strtrim(ext_dollar(s), "   ");
+    sub = getenv(ext + 1);
+    if (!sub)
+        sub = "";
+    //fprintf(stderr, "{%s: %s}\n", ext, sub);
+    return (sub);
 }
 
 /*
@@ -31,36 +43,25 @@ static int iterate_until_closing(char *s, char c)
 */
 char    *expand_utils(char *s)
 {
-    (void) iterate_until_closing;
     fprintf(stderr, "%s\n", s);
 
     int i;
     bool    sq;
     bool    wq;
-    int close;
-    bool    c;
 
     i = 0;
     sq = false;
     wq = false;
-    close = 0;
-    (void) close;
     while (s[i] != '\0')
     {
-        c = sq;
         if (s[i] == 39 || s[i] == 34)
-        {
             decide_word(s[i], &sq, &wq);
-            if (c && !sq)
-            {
-                //fprintf(stderr, "%c %s\n", s[i], c ? "true" : "false");
-                i += 1;
-                continue ;
-            }
-        }
         if (s[i] == '$' && !sq)
-            fprintf(stderr, "ext dollar: %s\n", ext_dollar(s + i));
-        //fprintf(stderr, "%c %s\n", s[i], sq ? "true" : "false");
+        {
+            sub_exp(s + i);
+            till_dollar(s, i);
+            fprintf(stderr, "join %s\n", ft_strjoin(till_dollar(s, i), sub_exp(s + i)));
+        }
         i++;
     }
 
