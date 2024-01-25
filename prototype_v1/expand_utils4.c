fabe1edecd8278cf61cb97c550518ebb0b8be614
isabella <isabella@42>
2024-01-25 12:02:18 +0800

    break expand_utils into a few functions to join instead of replace in place when i != 0

--- prototype_v1/expand_utils4.c
+++ prototype_v1/expand_utils4.c
@@ -6,7 +6,7 @@
 /*   By: maiman-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 14:34:58 by maiman-m          #+#    #+#             */
-/*   Updated: 2024/01/25 02:08:14 by maiman-m         ###   ########.fr       */
+/*   Updated: 2024/01/25 12:00:34 by maiman-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ static int iterate_until_closing(char *s, char c)
     return i;
 }
 
-static char    *partial_norme(char *ext, char *s, int start, int i, int *shift)
+static char    *partial_norme(char *ext, char *s, int start, int i)
 {
     char    *sub;
     char    *og;
@@ -40,8 +40,6 @@ static char    *partial_norme(char *ext, char *s, int start, int i, int *shift)
     if (!sub)
         sub = "";
     rep = repl(og, ext, sub, ft_strlen(og));
-    *shift = ft_strlen(sub) - ft_strlen(ext) + ft_strlen(og);
-    fprintf(stderr, "SHIFT %i\n", *shift);
     int end = i;
     if (end != 0)
         end += 1;
@@ -52,7 +50,7 @@ static char    *partial_norme(char *ext, char *s, int start, int i, int *shift)
     return (ret);
 }
 
-static char    *token_partial_repl(char *s, int i, int *close, int *shift)
+static char    *token_partial_repl(char *s, int i, int *close)
 {
     char    *ext;
     char    *og;
@@ -62,29 +60,40 @@ static char    *token_partial_repl(char *s, int i, int *close, int *shift)
     start = 0;
     *close = iterate_until_closing(og, s[i]);
     ext = ext_dollar(og);
-    return (partial_norme(ft_strtrim(ext, "     "), s, start, i, shift));
+    return (partial_norme(ft_strtrim(ext, "     "), s, start, i));
 }
 
-void	expand_utils(char **token, int shift)
+void	expand_utils(char **token, char *s, int call)
 {
+    (void) call;
+    (void) s;
         t_exp_norme exp_params;
-        int         i;
+        int i;
 
         exp_params = (t_exp_norme){0};
         exp_params.s = *token;
-        i = shift;
+        i = 0;
         while (exp_params.s[i] != '\0')
 	{
 		decide_word(exp_params.s[i], &exp_params.sq, &exp_params.wq);
                 if (!exp_params.sq && exp_params.s[i + 1] != '\0' && exp_params.s[i + 1] != 39)
                 {
-                    exp_params.part = token_partial_repl(exp_params.s, i, &exp_params.close, &shift);
-                    fprintf(stderr, "PARAM S %s\n", exp_params.s);
-                    fprintf(stderr, "PARAM S + i %s\n", exp_params.s + i);
-                    fprintf(stderr, "PARAM S + close %s\n", exp_params.s + exp_params.close);
-                    *token = exp_params.part;
-                    fprintf(stderr, "PARAM S + shift %s (%i)\n", exp_params.part + shift, shift);
-                    //expand_utils(&exp_params.part, shift);
+                    exp_params.part = token_partial_repl(exp_params.s, i, &exp_params.close);
+                    fprintf(stderr, "\x1b[43m PARAM part as is %s \x1b[m\n", exp_params.part);
+                    fprintf(stderr, "\x1b[42m *TOKEN as is %s \x1b[m\n", *token);
+                    if (call == 0)    
+                        *token = exp_params.part;
+                    s = *token;
+                    fprintf(stderr, "\x1b[42m *TOKEN replaced %s \x1b[m\n", *token);
+                    exp_params.part += 1;
+                    while (exp_params.part[0] != '\0' && exp_params.part[0] != '$')
+                        exp_params.part += 1;
+                    fprintf(stderr, "\x1b[43m PARAM part shifted %s \x1b[m\n", exp_params.part);
+                    fprintf(stderr, "\x1b[44m UNCHANGED S %s \x1b[m\n", s);
+                    //expand_utils(&exp_params.part, s, 1);
+                    int moved = ft_strlen(*token) - ft_strlen(exp_params.part);
+                    fprintf(stderr, "\x1b[44m SHIFTED S %s \x1b[m\n", s + moved);
+                    expand_utils(&exp_params.part, s, 1);
                     return ;
                 }
 		i++;
