cb27a123b796f4aa6929a10d93a9e630780c78d4
isabella <isabella@42>
2024-01-25 19:45:55 +0800

    expands sq at the end

--- prototype_v1/expand_utils4.c
+++ prototype_v1/expand_utils4.c
@@ -6,7 +6,7 @@
 /*   By: maiman-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 14:34:58 by maiman-m          #+#    #+#             */
-/*   Updated: 2024/01/25 14:17:49 by maiman-m         ###   ########.fr       */
+/*   Updated: 2024/01/25 19:43:11 by maiman-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ static int iterate_until_closing(char *s, char c)
     return i;
 }
 
-static char    *partial_norme(char *ext, char *s, int start, int i)
+static char    *partial_norme(char *ext, char *s, int start, int i, int *shift)
 {
     char    *sub;
     char    *og;
@@ -39,7 +39,11 @@ static char    *partial_norme(char *ext, char *s, int start, int i)
         og++;
     if (!sub)
         sub = "";
+    if (!ft_strcmp(ext, sub))
+        return (NULL);
     rep = repl(og, ext, sub, ft_strlen(og));
+    //*shift = ft_strlen(sub) - ft_strlen(og);
+    *shift = ft_strlen(sub);
     int end = i;
     if (end != 0)
         end += 1;
@@ -50,7 +54,7 @@ static char    *partial_norme(char *ext, char *s, int start, int i)
     return (ret);
 }
 
-static char    *token_partial_repl(char *s, int i, int *close)
+static char    *token_partial_repl(char *s, int i, int *close, int *shift)
 {
     char    *ext;
     char    *og;
@@ -60,7 +64,7 @@ static char    *token_partial_repl(char *s, int i, int *close)
     start = 0;
     *close = iterate_until_closing(og, s[i]);
     ext = ext_dollar(og);
-    return (partial_norme(ft_strtrim(ext, "     "), s, start, i));
+    return (partial_norme(ft_strtrim(ext, "     "), s, start, i, shift));
 }
 
 char    *utils(char *partial, char *og, int call)
@@ -75,56 +79,92 @@ char    *utils(char *partial, char *og, int call)
     return og;
 }
 
+char    *expand_utils(char **token, int call)
+{
+        (void) call;
+        t_exp_norme exp_params;
+        int i;
+        int shift;
+
+        exp_params = (t_exp_norme){0};
+        exp_params.s = *token;
+        i = 0;
+        shift = 0;
+        //fprintf(stderr, "START %s\n", exp_params.s);
+        if (!exp_params.s)
+            return (*token);
+        while (exp_params.s[i] != '\0' && ft_strchr(exp_params.s, '$'))
+	{
+		decide_word(exp_params.s[i], &exp_params.sq, &exp_params.wq);
+                if (!exp_params.sq && exp_params.s[i + 1] != '\0' && exp_params.s[i + 1] != 39)
+                {
+                    exp_params.part = token_partial_repl(exp_params.s, i, &exp_params.close, &shift);
+                    exp_params.s = exp_params.part;
+                    //fprintf(stderr, "TOKEN 1 %s\n", *token);
+                    if (!exp_params.s)
+                        return (*token);
+                    *token = exp_params.s;
+                    //fprintf(stderr, "TOKEN 2 %s\n", *token);
+                    //fprintf(stderr, "\x1b[43m PARAM part as is %s \x1b[m\n", exp_params.part);
+                    //fprintf(stderr, "END %s\n", exp_params.s);
+                    return (expand_utils(&exp_params.s, 0));
+                    return (*token);
+                }
+		i++;
+	}
+        return (*token);
+}
+
+/*
 void	expand_utils(char **token, int call)
 {
     (void) call;
         t_exp_norme exp_params;
         int i;
+        int shift;
         char    *s;
 
         exp_params = (t_exp_norme){0};
         exp_params.s = *token;
         s = NULL;
         i = 0;
+        shift = 0;
+    (void) s;
+        fprintf(stderr, "START %s\n", exp_params.s);
         while (exp_params.s[i] != '\0')
 	{
 		decide_word(exp_params.s[i], &exp_params.sq, &exp_params.wq);
                 if (!exp_params.sq && exp_params.s[i + 1] != '\0' && exp_params.s[i + 1] != 39)
                 {
-                    exp_params.part = token_partial_repl(exp_params.s, i, &exp_params.close);
-                    utils(exp_params.part, *token, call);
+                    exp_params.part = token_partial_repl(exp_params.s, i, &exp_params.close, &shift);
                     if (call == 0)
-                        *token = exp_params.part;
+                        exp_params.s = exp_params.part;
                     call += 1;
+                    fprintf(stderr, "SHIFT %i\n", shift);
                     fprintf(stderr, "\x1b[43m PARAM part as is %s \x1b[m\n", exp_params.part);
-                    s = ext_dollar(exp_params.part);
-                    if (s != NULL && ft_strlen(s) != 0)
-                    {
-                        *token = exp_params.part;
-                        fprintf(stderr, "\x1b[44m DOLLAR %s \x1b[m\n", s);
-                        exp_params.part = utils(s, *token, call);
-                        *token = exp_params.part;
-                    }
-                    fprintf(stderr, "\x1b[43m PARAM part as is %s \x1b[m\n", exp_params.part);
-                    s = ext_dollar(exp_params.part);
-                    if (s != NULL && ft_strlen(s) != 0)
-                    {
-                        *token = exp_params.part;
-                        fprintf(stderr, "\x1b[44m DOLLAR %s \x1b[m\n", s);
-                        exp_params.part = utils(s, *token, call);
-                        *token = exp_params.part;
-                    }
-                    fprintf(stderr, "\x1b[43m PARAM part as is %s \x1b[m\n", exp_params.part);
-                    s = ext_dollar(exp_params.part);
-                    if (s != NULL && ft_strlen(s) != 0)
-                    {
-                        *token = exp_params.part;
-                        fprintf(stderr, "\x1b[44m DOLLAR %s \x1b[m\n", s);
-                        exp_params.part = utils(s, *token, call);
-                        *token = exp_params.part;
-                    }
+                    fprintf(stderr, "END %s\n", exp_params.s + shift);
+                    
+                    //fprintf(stderr, "\x1b[43m PARAM part as is %s \x1b[m\n", exp_params.part);
+                    //s = ext_dollar(exp_params.part);
+                    //if (s != NULL && ft_strlen(s) != 0)
+                    //{
+                    //    *token = exp_params.part;
+                    //    fprintf(stderr, "\x1b[44m DOLLAR %s \x1b[m\n", s);
+                    //    exp_params.part = utils(s, *token, call);
+                    //    *token = exp_params.part;
+                    //}
+                    //fprintf(stderr, "\x1b[43m PARAM part as is %s \x1b[m\n", exp_params.part);
+                    //s = ext_dollar(exp_params.part);
+                    //if (s != NULL && ft_strlen(s) != 0)
+                    //{
+                    //    *token = exp_params.part;
+                    //    fprintf(stderr, "\x1b[44m DOLLAR %s \x1b[m\n", s);
+                    //    exp_params.part = utils(s, *token, call);
+                    //    *token = exp_params.part;
+                    //}
                     return ;
                 }
 		i++;
 	}
 }
+*/
--- prototype_v1/mini.h
+++ prototype_v1/mini.h
@@ -6,7 +6,7 @@
 /*   By: folim <folim@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 12:20:01 by maiman-m          #+#    #+#             */
-/*   Updated: 2024/01/25 13:08:49 by maiman-m         ###   ########.fr       */
+/*   Updated: 2024/01/25 19:33:08 by maiman-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -268,7 +268,7 @@ int             unterminated_quotes(t_token *t_node, t_status *stat);
 void		expansion(t_token **tokens);
 
 //expand_utils4.c
-void		expand_utils(char **token, int call);
+char            *expand_utils(char **token, int call);
 
 //expand_utils5.c
 void            decide_word(char c, bool *sq, bool *wq);
